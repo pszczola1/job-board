1d206ca37ddec558353362c9324b62b7aee97c41
pszczola <181292276+pszczola1@users.noreply.github.com>
2024-09-14 00:43:03 +0200

    Add correct plural verbose name for Category model

--- job_offers/models.py
+++ job_offers/models.py
@@ -23,6 +23,10 @@ class Category(models.Model):
 
     def __str__(self) -> str:
         return self.name
+    
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
 
 
 class JobListing(models.Model):
