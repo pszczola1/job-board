40d7cbfc34d4a1290263b9cefc7ae55d9320a6d0
pszczola <181292276+pszczola1@users.noreply.github.com>
2024-09-16 20:47:54 +0200

    Add api/v1/listings viewset
    Added api/v1/listings(/get_page/?page=X) viewset with serialization and pagination

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
+router.register(r'listings', views.ListingsViewset, basename="listings-api")
 
 urlpatterns = [
     path(r'api/v1/', include(router.urls))
--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import ListingsGetPageSerializer
+
+from job_offers.models import JobListing
+
+class ListingsViewset(viewsets.ModelViewSet):
+
+    serializer_class = ListingsGetPageSerializer
+    queryset = JobListing.objects.all()
+
+    @action(methods=['get'], detail=False)
+    def get_page(self, request):
+        queryset = JobListing.objects.all()
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        
