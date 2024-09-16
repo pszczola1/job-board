dc34ff6fd59d6a9c56942f1daa66a4573836a537
pszczola <181292276+pszczola1@users.noreply.github.com>
2024-09-16 19:35:51 +0200

    Removed base for listings page
    Removed base for listings page, because I decided to send offers via an api call using djangorestframework

--- job_offers/views.py
+++ job_offers/views.py
@@ -1,8 +1,6 @@
-from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import JobListingCreationForm
-from .models import JobListing
 
 # Create your views here.
 
@@ -18,12 +16,7 @@ def create_listing(request):
     return render(request, "job_offers/create_listing.html", context=context)
 
 def listings(request):
-    page_number = request.GET.get("page")
-
-    listings = JobListing.objects.all()
-    paginator = Paginator(listings, 10)
-    page_obj = paginator.get_page(page_number)
-    return render(request, "job_offers/listings.html", {'page_obj': page_obj})
+    return render(request, "job_offers/listings.html")
 
 def listing(request, id):
     ...
