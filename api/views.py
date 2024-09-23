3cdf324367969c8f2664c6a210dd06bd150c5684
pszczola <181292276+pszczola1@users.noreply.github.com>
2024-09-23 17:46:11 +0200

    Added filtering for all fieldss
    A listing must have all selected categories to pass the filter, but it must have only one of selected work model and employment type to pass

--- api/views.py
+++ api/views.py
@@ -18,14 +18,37 @@ class ListingsViewset(viewsets.ModelViewSet):
         title = self.request.query_params.get('title')
         categories = self.request.query_params.getlist('categories')
         salary = self.request.query_params.get('salary')
+        location = self.request.query_params.get('location')
+        employment_types = self.request.query_params.getlist('employment_type')
+        work_models = self.request.query_params.getlist('work_model')
+
+        #for categories, a listing must have all categories to pass the filter
+        #for employment_type and work_models, it must have only one
 
         if title:
             queryset = queryset.filter(title__icontains=title)  #icontains - not case sensitive
+
         if categories:
             for category in categories:
                 queryset = queryset.filter(categories__in=category)
+
         if salary:
             queryset = queryset.filter(salary__gt=int(salary)-1)
+
+        if location:
+            queryset = queryset.filter(location__icontains=location)
+
+        if employment_types: 
+            filter_arr = []
+            for employment_type in employment_types:
+                filter_arr.append(employment_type)
+            queryset = queryset.filter(employment_type__in=filter_arr) 
+
+        if work_models:
+            filter_arr = []
+            for work_model in work_models:
+                filter_arr.append(work_model)
+            queryset = queryset.filter(work_model__in=filter_arr)
 
         print(queryset)
         return queryset
