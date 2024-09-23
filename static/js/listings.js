f9f3a6f77368a5246c2f887e90b06525ea6ea45a
pszczola <181292276+pszczola1@users.noreply.github.com>
2024-09-23 17:17:09 +0200

    Added filter form logic

--- static/js/listings.js
+++ static/js/listings.js
@@ -1,12 +1,18 @@
-const fetchListings = async () => {
+const fetchListings = async (isInitial=false, filters=[]) => {
     let pageNumber = (new URLSearchParams(window.location.search)).get("page")
-    
     let testNumRegex = /^\d+$/m
     if(!testNumRegex.test(pageNumber)) pageNumber = 1
-     
-    const response = await fetch(`${window.location.origin}/api/v1/listings/?page=${pageNumber}`)
-    const listings = await response.json()
-    return listings   
+
+    let response
+    if(isInitial) response = await fetch(`${window.location.origin}/api/v1/listings/?page=${pageNumber}`)
+    else {
+        let apiUrl = `${window.location.origin}/api/v1/listings/?page=${pageNumber}`
+        for (const filter of filters) {
+            apiUrl += `&${filter[0]}=${filter[1]}`
+        }
+        response = await fetch(apiUrl)
+    }
+    return response.json()
 }
 
 
@@ -54,10 +60,36 @@ const buildPage = (listings) => {
     }
 }
 
+const clearPage = () => {
+    const listingsWrapper = document.querySelector(".listings")
+    listingsWrapper.innerHTML = ""
+}
+
 
 const main = async () => {
-    const response = await fetchListings()
+    const response = await fetchListings(isInitial=true)
     buildPage(response.results)
+    const filterBtn = document.querySelector(".filter-submit-btn")
+    filterBtn.addEventListener("click", handleFilterBtn)
+}
+
+const handleFilterBtn = async (e) => {
+    e.preventDefault()
+    const formData = getFilterFormData()
+    let filters = []
+    for (const element of formData.entries()) {
+        if(element[1] == "") continue
+        filters.push([element[0], element[1]])
+    }
+    const response = await fetchListings(isInitial=false, filters=filters)
+    clearPage()
+    buildPage(response.results)
+}
+
+const getFilterFormData = () => {
+    const form = document.querySelector(".filter-form")
+    const formData = new FormData(form)
+    return formData
 }
 
 window.addEventListener("DOMContentLoaded", main)
